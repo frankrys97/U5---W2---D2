62f41373e952645cb1792fb54f7d1da81c56fd39
Francesco Cristiano <frankrys97@gmail.com>
2024-06-25 13:12:35 +0200

    adds faker

--- src/main/java/francescocristiano/U5_W2_D2/entities/BlogPost.java
+++ src/main/java/francescocristiano/U5_W2_D2/entities/BlogPost.java
@@ -9,7 +9,7 @@ import java.util.Random;
 @Data
 public class BlogPost {
     private int id;
-    private Category categoria;
+    private Category category;
     private String title;
     private String cover;
     private String body;
@@ -17,7 +17,7 @@ public class BlogPost {
 
     public BlogPost(String categoryString, String title, String body, int readingTime) {
         this.id = new Random().nextInt(1, 10000);
-        this.categoria = this.categoryFromString(categoryString);
+        this.category = this.categoryFromString(categoryString);
         this.title = title;
         this.cover = "https://picsum.photos/200/300";
         this.body = body;
--- src/main/java/francescocristiano/U5_W2_D2/services/BlogPostService.java
+++ src/main/java/francescocristiano/U5_W2_D2/services/BlogPostService.java
@@ -1,6 +1,7 @@
 package francescocristiano.U5_W2_D2.services;
 
 
+import com.github.javafaker.Faker;
 import francescocristiano.U5_W2_D2.entities.BlogPost;
 import francescocristiano.U5_W2_D2.exeptions.NotFoundExpetion;
 import org.springframework.stereotype.Service;
@@ -15,8 +16,13 @@ public class BlogPostService {
 
     private List<BlogPost> blogPosts = new ArrayList<>();
     private Random random = new Random();
+    private Faker faker = new Faker();
+
 
     public BlogPost saveBlogPost(BlogPost blogPost) {
+        blogPost.setTitle(faker.book().title());
+        blogPost.setBody(faker.lorem().paragraph(random.nextInt(1, 3)));
+        /*blogPost.setCategory(Category.values()[random.nextInt(0, Category.values().length)]);*/
         blogPosts.add(blogPost);
         return blogPost;
     }
@@ -35,7 +41,7 @@ public class BlogPostService {
         found.setTitle(blogPostUpdated.getTitle());
         found.setBody(blogPostUpdated.getBody());
         found.setReadingTime(blogPostUpdated.getReadingTime());
-        found.setCategoria(blogPostUpdated.getCategoria());
+        found.setCategory(blogPostUpdated.getCategory());
         found.setCover(blogPostUpdated.getCover());
         return found;
     }
